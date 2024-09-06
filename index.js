adeceba23699f6a11f196182544b1dedad7ae2cf
Nugar <cladeadenugar@gmail.com>
2024-09-06 23:33:17 +0100

    added chat function

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import http from "http";
 
-//App configuration
+// App configuration
 const app = express();
 app.use(express.json());
 app.use(
@@ -18,19 +20,48 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 dotenv.config();
 
-//routes
+// Routes
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 app.use("/posts", postRoutes);
 app.use("/", userRoutes);
 
-//mongoDB configuration
+// MongoDB configuration
 const MONGODB_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 8080;
 
 mongoose
   .connect(MONGODB_URL)
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server open in port: ${PORT}`))
-  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    server.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
   .catch((error) => console.log(error));
+
+// HTTP server
+const server = http.createServer(app);
+
+// Socket.io setup
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+// Socket.io connection
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("chatMessage", (message) => {
+    io.emit("chatMessage", message);
+  });
+
+  // user disconnect
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
