7a09ebc7b08a5c14e0e7165830cb7b7544ea9520
Nugar <cladeadenugar@gmail.com>
2024-09-27 14:50:58 +0100

    new

--- controllers/message.js
+++ controllers/message.js
@@ -1,17 +1,28 @@
 import Message from "../models/Message.js";
+import Conversation from "../models/Conversation.js";
+import { io } from "../index.js";
 
-//send message
+// Send message
 export const sendMessage = async (req, res) => {
   const newMessage = new Message(req.body);
+
   try {
     const savedMessage = await newMessage.save();
+
+    await Conversation.findByIdAndUpdate(
+      savedMessage.conversationId,
+      { updatedAt: Date.now() },
+      { new: true }
+    );
+
     res.status(200).json(savedMessage);
+    io.emit("newMessage", savedMessage);
   } catch (err) {
     res.status(500).json({ message: "Failed to send message" });
   }
 };
 
-//get message
+// Get messages
 export const getMessages = async (req, res) => {
   try {
     const messages = await Message.find({
